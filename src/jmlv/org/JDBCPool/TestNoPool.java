a30a76d7edc3e57beecaed13197110cce1e66f5c
Jose Lopez <joselopezjm12@hotmail.com>
2016-09-29 15:57:30 -0400

    NoPool

---
+++ src/jmlv/org/JDBCPool/TestNoPool.java
@@ -0,0 +1,56 @@
+package jmlv.org.JDBCPool;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import jmlv.org.jbuilder.JBuilder;
+import jmlv.org.jdbconnection.JDBConnection;
+
+public class TestNoPool implements Runnable {
+	private Thread t;
+	private String threadName;
+	private Integer priority;
+	private static Integer max = 80;
+
+	TestNoPool(String name, Integer Priority) {
+		threadName = name;
+		priority = Priority;
+		System.out.println("Creating " + threadName);
+	}
+
+	public void run() {
+		long startTime = System.currentTimeMillis();
+		JDBConnection w = new JDBConnection();
+		Timer time = new Timer();
+		try {
+			w.setConnection("localhost", "5432", "framework", "postgres", "masterkey");
+			w.executeQuery("SELECT * FROM usuarios");
+			long endTime = System.currentTimeMillis();
+			Long x = (long) endTime - startTime;
+			time.setTime(x);
+		} catch (Exception e) {
+			System.out.println("-------ERROR!! Total Time " + time.getTime() + "ms");
+		}
+		System.out.println("-------Total Time " + priority + " " + time.getTime() + "ms");
+	}
+
+	public void start() {
+		System.out.println("Starting " + threadName);
+		if (t == null) {
+			t = new Thread(this, threadName);
+			t.start();
+		}
+	}
+
+	public static void main(String args[]) throws SQLException, IOException {
+
+		for (int i = 0; i < max; i++) {
+			TestNoPool R1 = new TestNoPool("Thread " + i, i);
+			R1.start();
+		}
+
+	}
+
+}
